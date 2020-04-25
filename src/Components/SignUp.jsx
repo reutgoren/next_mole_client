7ee2ddc8c145c463c1dc15359d826e94879edfc8
galvak16 <49720230+galvak16@users.noreply.github.com>
2020-04-25 15:53:12 +0300

    sign up validtion

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -4,6 +4,7 @@ import { withRouter } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
 import '../css/index.css';
+import { csvParse } from "d3";
 
 const MySwal = withReactContent(Swal)
 
@@ -24,7 +25,9 @@ class SignUp extends Component {
           userPassword: '',
           userName:'',
           //userBirthdate:'',
-          userGender:''
+          userGender:null,
+          validation:'True'
+
         }
         this.handleEmailChange = this.handleEmailChange.bind(this);
         this.handlePasswordChange = this.handlePasswordChange.bind(this);
@@ -35,7 +38,7 @@ class SignUp extends Component {
       }
 
       handleEmailChange(event) {
-        this.setState({ userEmail: event.target.value });
+        this.setState({ userEmail: event.target.value });    
       }
 
       handlePasswordChange(event) {
@@ -55,8 +58,34 @@ class SignUp extends Component {
       }
             
     signUser = (event) => {     //להוסיף ולידציה, שלא יהיה אפשר לאפשר את הטופס מבלי למלא את השדות כראוי
+      
+      var counter=0;
+      if (/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}$/i.test(this.state.userEmail)) {    
+          counter++;
+      }
+      else{
+        this.setState({validation: 'False'})
+      }     
+      var pass=this.state.userPassword;
+      var reg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,32}$/;
+      var test = reg.test(pass);
+        if (test) {
+         counter++;
 
-
+        } else{
+            this.setState({validation: 'False'})
+            MySwal.fire("Your password must contain at least one uppercase and must be between 8 and 30 characters, please try again", "", "warning")
+        }
+        
+      if(this.state.userGender===null){
+        this.setState({validation: 'False'})
+        MySwal.fire("select a Gender, please try again", "", "warning")
+      }
+      else{
+          counter++;
+      }
+      console.log(counter);
+   if(counter===3){
         event.preventDefault();
         const userToPost={
             UserEmail: this.state.userEmail,
@@ -90,6 +119,7 @@ class SignUp extends Component {
               console.log("err post=", error);
             });
       }
+    }
 
     render() {
         return (
@@ -107,7 +137,7 @@ class SignUp extends Component {
                         </div>
                         <div className="form-group">
                             <label>User name</label>
-                            <input type="text" onChange={this.handleUserNameChange} className="form-control" placeholder="Enter user name" />
+                            <input type="text" onChange={this.handleUserNameChange} className="form-control" placeholder="Full Name" />
                         </div>
                         {/*
                         <div className="form-group">
